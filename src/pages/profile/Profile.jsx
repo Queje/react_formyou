e3e370c5d8a1dc53ba37cc8e7294626a0694cded
Quentin PLAUD <qplaud@gmail.com>
2021-03-09 12:20:16 +0100

    1st step of admin features --> Verify data transmission regarding roles all the way towards user profile

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import ProfileDisplay from "../../components/profile/ProfileDisplay";
+
+const Profile = () => {
+  const currentUser = useSelector((state) => state.auth.currentUser);
+
+  return (
+    <div className="row my-3">
+      <div className="col-4">
+        <ProfileDisplay data={currentUser} />
+        <div className="TODO --> Component / Page Edit profile entries">
+        </div>
+      </div>
+
+      <div className="col-8 TODO --> List of booked training for the given user?">
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
