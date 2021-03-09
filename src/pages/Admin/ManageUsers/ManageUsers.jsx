c5192003d7db70f8277abaa94ef9a3b96456efc9
cha-fa <favier.cha@gmail.com>
2021-03-09 15:31:25 +0100

    Add loading to page

--- src/pages/Admin/ManageUsers/ManageUsers.jsx
+++ src/pages/Admin/ManageUsers/ManageUsers.jsx
@@ -3,6 +3,7 @@ import "./ManageUsers.scss";
 import useFetch from "hooks/useFetch";
 import { Table } from "react-bootstrap";
 import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
+import Loading from "components/Loading";
 
 const ManageUsers = () => {
   const { data, error, isLoading, get, patch } = useFetch();
@@ -20,46 +21,50 @@ const ManageUsers = () => {
   return (
     <div className="ManageUsers">
       {error && <h4>{error}</h4>}
-      {data && <p>Users pending for review : {data.length}</p>}
-      {data && (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th colSpan="2">Approve user?</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.email}</td>
-                <td>{user.role}</td>
-                <td>
-                  <AiFillCheckCircle
-                    size={30}
-                    style={{ color: "green" }}
-                    onClick={() => editUser(user.id, true)}
-                  />
-                </td>
-                <td>
-                  <AiFillCloseCircle
-                    size={30}
-                    style={{ color: "red" }}
-                    onClick={() => editUser(user.id, false)}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      )}
+      {(isLoading && <Loading />) ||
+        (data && (
+          <>
+            <h2>Users pending for review : {data.length}</h2>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th colSpan="2">Approve user?</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                    <td>
+                      <AiFillCheckCircle
+                        size={30}
+                        style={{ color: "green" }}
+                        onClick={() => editUser(user.id, true)}
+                      />
+                    </td>
+                    <td>
+                      <AiFillCloseCircle
+                        size={30}
+                        style={{ color: "red" }}
+                        onClick={() => editUser(user.id, false)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </>
+        ))}
     </div>
   );
 };
