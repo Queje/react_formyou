8f933498b64c7698fd0a221acc28eadd7d72568a
Dan <dan.bertrand.hs@gmail.com>
2021-03-09 10:25:04 +0100

    Add react-big-calendar + Moment to dependency + Create a basic calendar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import PromotionsCalendar from '../components/promotionsCalendar/index.jsx';
 
 const Home = () => {
   return (
     <div className="Home">
       <h1>This is home</h1>
+      <PromotionsCalendar />
     </div>
   );
 };
