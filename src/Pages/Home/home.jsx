94c831c7e08522f3be7313f4535cb97067977fe5
Dan <dan.bertrand.hs@gmail.com>
2021-03-09 17:59:58 +0100

    Add dynamics routes for Courses with calendar showing the Promotions, cleaned the folder and files

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CourseList from '../CourseList/courseList.jsx';
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>This is home</h1>
+      <CourseList />
+    </div>
+  );
+};
+
+export default Home;
