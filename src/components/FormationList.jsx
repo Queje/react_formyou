cc2cc9e58b3af685f5c2c33855d38c44a721eda7
Paul K <azn.baoul@gmail.com>
2021-03-09 21:54:58 +0100

    add use of useFetch hook

--- src/components/FormationList.jsx
+++ src/components/FormationList.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from "react";
+import useFetch from "hooks/useFetch";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { getCourses } from "services/coursesService";
 import CourseCard from "./courseCard";
 
 const FormationList = (props) => {
+  const { data, get } = useFetch();
   const currentUser = useSelector((state) => state.auth.currentUser);
-  const [courses, setCourses] = useState([]);
-  const fetchCourse = async () => {
-    const courses = await getCourses();
-    setCourses(
-      courses.filter((course) => course.teacher_id === currentUser.id)
-    );
-  };
-  useEffect(() => fetchCourse(), []);
+
+  useEffect(() => {
+    get(`/courses?teacher_id=${currentUser.id}`);
+  }, []);
   return (
-    <div>
+    <div className="col-12">
       <h1>Vous êtes assigné à ces cours :</h1>
       <div className="row">
-        {courses.map((course) => {
+        {data?.map((course) => {
           return <CourseCard key={course.id} course={course} />;
         })}
       </div>
