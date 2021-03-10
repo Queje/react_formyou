0fe40de5806241f832dface85cb5b072d918717f
Dan <dan.bertrand.hs@gmail.com>
2021-03-10 12:17:50 +0100

    save before merge

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -9,13 +9,15 @@ const Course = ({ match }) => {
 
   const getCourse = async () => {
     const courses = await getCourses();
-    const courseToShow = await courses.filter((c) => c.id === match.params.id);
+    const courseToShow = await courses.filter((c) => c.id === parseInt(match.params.id));
+    console.log("match.params.id",typeof match.params.id)
     setCourse(await courseToShow[0]);
   };
   const fetchPromotions = async () => {
     const promotions = await getPromotions();
+
     const promotionToShow = await promotions.filter(
-      (p) => p.course_id === match.params.id
+      (p) => p.course_id === parseInt(match.params.id)
     );
     setPromotions(promotionToShow);
   };
