eb0ef1e240168119ee1aafe89668f13ec2d37e73
Dan <dan.bertrand.hs@gmail.com>
2021-03-10 17:25:06 +0100

    save change before merging User Delete feeature

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const CourseCard = ({course}) => {
+const CourseCard = ({course, editCourse, deleteCourse}) => {
+
+  const location = useLocation();
+
+  const currentUser = useSelector((state) => state.auth.currentUser);
+
+  console.log("Current user", currentUser.role)
+  console.log("Location", location)
 
   return (
     <div className="col-6 col-md-4">
@@ -9,7 +17,14 @@ const CourseCard = ({course}) => {
         <div className="card-body">
           <h5 className="card-title">{course.title}</h5>
           <p className="card-text">{course.content}</p>
-          <Link to={`/courses/${course.id}`}>See sessions</Link>
+         {location.pathname === "/" && 
+              <Link to={`/courses/${course.id}`}>See sessions</Link>
+          || 
+            <>
+              <button onClick={(e) => editCourse(course.id)} type="type" >Update</button>
+              <button onClick={(e) => deleteCourse(course.id)} type="button" >Delete</button>
+            </>
+          }
         </div>
       </div>
     </div>
--- src/pages/Admin/ManageCourses/ManageCourses.jsx
+++ src/pages/Admin/ManageCourses/ManageCourses.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import useFetch from "Hooks/useFetch";
 import "./ManageCourses.scss";
+import CourseCard from 'components/CourseCard/CourseCard';
 const ManageCourses = () => {
+  const { data, error, isLoading, get, patch } = useFetch();
+  const [courses, setCourses] = useState([]);
+  // const [courses, setCourses] = useState([]);
+
+  const editCourse = (id) => {console.log("edit", id)};
+
+
+  const deleteCourse = (id) => {
+    delete("/courses/:id");
+  };
+
+  const getCourse = async () => {
+    await get("/courses");
+    setCourses(data);
+  }
+
+
+  useEffect(() => {getCourse()}, []);
+
+  console.log("courses", courses)
+
   return (
-    <div className="ManageCourses">This is where we'll manage courses</div>
-  );
+    <div className="ManageCourses">
+    <h1>This is where we'll manage courses</h1>
+      <div className="courses" >
+        <h2>Courses</h2>
+          {courses && (
+            <div className="row">
+              {courses.map((e) => (
+                <CourseCard key={e.id} course={e} editCourse={editCourse} deleteCourse={deleteCourse} />
+              ))}
+            </div>
+          )}
+      </div>
+    </div>
+    );
 };
 
 export default ManageCourses;
